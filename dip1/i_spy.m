409dbc6e7b46ce1c720523aef2e79dedaebaaf2f
Kexin Ding <ked317@lehigh.edu>
2019-09-10 14:30:57 -0400

    update

--- dip1/i_spy.m
+++ dip1/i_spy.m
@@ -2,21 +2,38 @@
 % along with your PDF report
 
 function [r,c] = i_spy (object_im, big_im)
+Rp = size(big_im, 1);
+Cp = size(big_im, 2);
+
+if Rp<700
+    if Cp<700
+        object_grey = double(rgb2gray(object_im));
+        object_grey = imresize(object_grey,0.6);
+        big_grey = double(rgb2gray(big_im));
+        big_grey = imresize(big_grey,0.6);
+        flag = 0;
+    end
+else
+    object_grey = double(rgb2gray(object_im));
+    object_grey = imresize(object_grey,0.25);
+    big_grey = double(rgb2gray(big_im));
+    big_grey = imresize(big_grey,0.25);
+    flag = 1;
+end
 %object_im = Object image that needs to be detected
 %big_im = Image in which detections needs to be done.
 
-Ro = size(object_im, 1);
-Co = size(object_im, 2);
 
-Rb = size(big_im, 1);
-Cb = size(big_im, 2);
+Ro = size(object_grey, 1);
+Co = size(object_grey, 2);
+
+Rb = size(big_grey, 1);
+Cb = size(big_grey, 2);
 
 r=0;c=0;
 R_crop = 0;
 R = 0;
 
-object_grey = double(rgb2gray(object_im));
-big_grey = double(rgb2gray(big_im));
 for i = 1:Rb-Ro
     for j = 1:Cb-Co
         crop_arr = big_grey(i:i-1+Ro,j:j-1+Co);
@@ -25,13 +42,19 @@ for i = 1:Rb-Ro
         %object_grey = object_grey - mean2(object_grey);
         R_crop = sum(sum(object_grey.*crop_arr))/(sum(sum(object_grey.*object_grey))*sum(sum(crop_arr.*crop_arr)))^0.5;
         %R = corr2(object_grey,crop_arr);
-        if R_crop >0.95
-            r = i;
-            c = j;
-            break
-
+        if R_crop >0.9
+            if flag == 0
+                r = i/0.6;
+                c = j/0.6;
+                break
+            else    
+                r = i/0.25;
+                c = j/0.25;
+                break
+            end
         end
      end
 end
 return;
 end
+
