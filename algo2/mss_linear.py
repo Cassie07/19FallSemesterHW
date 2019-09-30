0ac68ce82d5adb013fce5763feb3dc72d338b006
Kexin Ding <ked317@lehigh.edu>
2019-09-29 22:55:40 -0400

    upload

--- algo2/mss_linear.py
+++ algo2/mss_linear.py
@@ -2,21 +2,21 @@ def find_max_subarray(arr, start, end):
     """Returns (l, r, m) such that arr[l:r] is the maximum subarray in
     A[start:end] with sum m. Here A[start:end] means all A[x] for start <= x <
     end."""
-    max_ending_at_i = max_seen_so_far = arr[start]
-    max_left_at_i = max_left_so_far = start
+    max_ending_at_i = tmp_max = arr[start]
+    max_left_at_i = tmp_max_left = start
     # max_right_at_i is always i + 1
-    max_right_so_far = start + 1
+    tmp_max_right = start + 1
     for i in range(start + 1, end):
         if max_ending_at_i > 0:
             max_ending_at_i += arr[i]
         else:
             max_ending_at_i = arr[i]
             max_left_at_i = i
-        if max_ending_at_i > max_seen_so_far:
-            max_seen_so_far = max_ending_at_i
-            max_left_so_far = max_left_at_i
-            max_right_so_far = i + 1
-    return max_left_so_far, max_right_so_far, max_seen_so_far
+        if max_ending_at_i > tmp_max:
+            tmp_max = max_ending_at_i
+            tmp_max_left = max_left_at_i
+            tmp_max_right = i + 1
+    return tmp_max_left, tmp_max_right, tmp_max
 
 
 arr = input('Enter the list of numbers: ')
